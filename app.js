6a96653b1d69a85c7f6e01f1b32eba8eba10ab96
Hussainarbab <arbabhussan63@gmail.com>
2024-10-01 17:41:37 +0500

    commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// alert("Do you want to visit my portifolio");
+
+// function openNav() {
+//     document.getElementById("mySidebar").style.width = "250px"; 
+//     document.getElementById("main").style.marginLeft = "250px";
+// }
+
+// function closeNav() {
+//     document.getElementById("mySidebar").style.width = "0";
+//     document.getElementById("main").style.marginLeft = "0";
+// }
